d28c0565f0fd3dbc5d428c7134662976aa99d42b
andriyomelchenko <andriy.omelchenko@agilie.com>
2023-04-19 23:55:21 +0300

    init

--- src/app/konva/components/konva/konva.component.ts
+++ src/app/konva/components/konva/konva.component.ts
@@ -2,6 +2,7 @@ import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {Stage} from "konva/lib/Stage";
 import {Layer} from "konva/lib/Layer";
 import {KonvaService} from "../../service/konva.service";
+import {FileSystemDirectoryEntry, FileSystemFileEntry, NgxFileDropEntry} from "ngx-file-drop";
 
 enum EKonvaButton {
   Line = 'line',
@@ -12,171 +13,100 @@ enum EKonvaButton {
   Pencil = 'brush'
 }
 
+interface Folder {
+  name: string;
+  file?: File;
+  children: Folder[];
+}
+
+const acceptedGlobalExtensions =
+  '.doc .pptx .docx .pdf .xls .xlsx .ppt .png .jpg .jpeg .dng .tiff .heic .txt .xlsm .pages .ai .psd .tiff .dxf .svg .zip .rar .mov .qt .mp4 .avi .mkv';
+
 @Component({
   selector: 'app-konva',
   templateUrl: './konva.component.html',
   styleUrls: ['./konva.component.scss']
 })
-export class KonvaComponent implements OnInit {
-  container = 'konva-container';
-  shapes: any = [];
-  stage!: Stage;
-  layer!: Layer;
-  inkColor: string = '#000000';
-  controlButton = EKonvaButton;
-  activeButton = '';
-  selectedButton: any = {
-    [this.controlButton.Line]: false,
-    [this.controlButton.Eraser]: false,
-    [this.controlButton.Arrow]: false,
-    [this.controlButton.Circle]: false,
-    [this.controlButton.Square]: false,
-    [this.controlButton.Pencil]: false,
-  }
-  eraser: boolean = false;
-  transformers: Transformer[] = [];
-  brushSize: number = 3;
-  brushOpacity: number = 1.0;
-  background = 'https://www.houseplanshelper.com/images/how-to-read-floor-plans-full-floor-plan.jpg';
+export class KonvaComponent {
 
-  @ViewChild('konvaContainer', {static: true}) konvaContainer!: ElementRef;
+  public files: NgxFileDropEntry[] = [];
+  accept = acceptedGlobalExtensions;
+  folders: Folder[] = [];
 
-  constructor(
-    private konvaService: KonvaService
-  ) { }
+  drop(files: NgxFileDropEntry[]): void {
+    this.files = files;
+    for (const droppedFile of files) {
 
-  ngOnInit(): void {
-    this.stage = new Stage({
-      container: this.container,
-      width: this.konvaContainer.nativeElement.offsetWidth,
-      height: this.konvaContainer.nativeElement.offsetHeight,
-    });
-    this.stage.toDataURL()
-    this.layer = new Layer();
-    this.stage.add(this.layer);
-    this.addLineListeners();
-  }
+      const adr = droppedFile.relativePath.split('/');
+      adr.pop();
 
-  clearSelection(button: string): void {
-    Object.keys(this.selectedButton).map(key => {
-      if (key === button) return
-      this.selectedButton[key] = false
-    });
-  }
+      if (droppedFile.fileEntry.isFile) {
+        const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
+        fileEntry.file((file: File) => {
+          // .....
 
-  setSelection(key: string) {
-    this.activeButton = key;
-    this.clearSelection(key);
-    this.selectedButton[key] = !this.selectedButton[key];
-  }
-
-  addLineListeners(): void {
-    const component = this;
-    let lastLine: any;
-    let isPaint: boolean = false;
-    this.stage.on('mousedown touchstart', function () {
-      if (!component.activeButton) {
-        return;
-      }
-      isPaint = true;
-      let pos = component.stage.getPointerPosition();
-      Object.keys(component.selectedButton).map(key => {
-        if (component.selectedButton[key]) {
-          // @ts-ignore
-          lastLine = component.konvaService[key](
-            pos,
-            component.brushSize,
-            component.inkColor,
-            component.brushOpacity
-          )
+          if (adr?.length) {
+            this.searchFolder(this.folders, adr, file)
+          } else {
+            this.folders.push({
+              name: file.name,
+              file,
+              children: []
+            })
+          }
+        });
+      } else {
+        if (adr?.length) {
+          this.searchFolder(this.folders, adr)
         }
-      })
-
-      component.shapes.push(lastLine);
-      component.layer.add(lastLine);
-    });
-
-    this.stage.on('mouseup touchend', function () {
-      isPaint = false;
-    });
-
-    this.stage.on('mousemove touchmove', function (e) {
-      if (!isPaint) {
-        return;
       }
-      if (!component.activeButton) {
-        return;
-      }
-      let newPoints: any;
-      e.evt.preventDefault();
-      const position: any = component.stage.getPointerPosition();
-      const attr = lastLine.getAttrs();
-
-      switch (component.activeButton) {
-        case component.controlButton.Square:
-          const width = position.x - attr.x;
-          const height = position.y - attr.y;
-          lastLine.width(width);
-          lastLine.height(height);
-          break;
-        case component.controlButton.Circle:
-          const offsetX = attr.x - position.x;
-          const offsetY = attr.y - position.y;
-          const radius = Math.sqrt(Math.pow(offsetX, 2) + Math.pow(offsetY, 2))/2;
-          lastLine.radius(radius)
-          lastLine.offsetX(offsetX/2);
-          lastLine.offsetY(offsetY/2);
-          break;
-        case component.controlButton.Line:
-          const point = lastLine.points();
-          point[2] = position.x;
-          point[3] = position.y;
-          lastLine.points(point);
-          break;
-        case component.controlButton.Arrow:
-          const pointArrow = lastLine.points();
-          pointArrow[2] = position.x;
-          pointArrow[3] = position.y;
-          lastLine.points(pointArrow);
-          break;
-        default:
-          newPoints = lastLine.points().concat([position.x, position.y]);
-          lastLine.points(newPoints);
-      }
-
-      component.layer.batchDraw();
-    });
-  }
-
-  undo(): void {
-    const removedShape = this.shapes.pop();
-
-    this.transformers.forEach(t => {
-      // @ts-ignore
-      t.detach();
-    });
-
-    if (removedShape) {
-      removedShape.remove();
     }
 
-    this.layer.draw();
+    console.log('FOLDER', this.folders);
   }
 
-  clearBoard(): void {
-    this.layer.destroyChildren();
-    this.layer.draw();
-  }
 
-  saveAsImage(): void {
-    console.log(this.stage.toObject())
-  }
+  searchFolder(folder: Folder[], folderStructure: string[], file?: File): void {
+    if (folderStructure[0]) {
+      const name = folderStructure[0];
 
-  get getCursorClass(): string {
-    if (this.selectedButton['brush'] || this.selectedButton['eraser']) {
-      return 'pointer_cursor';
-    } else {
-      return 'default';
+      const idx = folder.findIndex(el => el.name === name);
+
+      if (idx === -1) {
+        const l = folder.push({
+          name: name,
+          children: []
+        });
+
+        if (folderStructure.length === 1) {
+          if (file) {
+            folder[l - 1].children.push({
+              name: file?.name,
+              file: file,
+              children: []
+            })
+          }
+        }
+
+        if (folderStructure.slice(1).length) {
+          this.searchFolder(folder[l - 1].children, folderStructure.slice(1), file);
+        }
+      } else {
+
+        if (folderStructure.length === 1) {
+          if (file) {
+            folder[idx].children.push({
+              name: file?.name,
+              file: file,
+              children: []
+            })
+          }
+        }
+
+        if (folderStructure.slice(1).length) {
+          this.searchFolder(folder[idx].children, folderStructure.slice(1), file);
+        }
+      }
     }
   }
 }
--- src/app/konva/konva.module.ts
+++ src/app/konva/konva.module.ts
@@ -3,14 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { KonvaRoutingModule } from './konva-routing.module';
 import { KonvaComponent } from './components/konva/konva.component';
+import {NgxFileDropModule} from "ngx-file-drop";
 
 @NgModule({
   declarations: [
     KonvaComponent
   ],
-  imports: [
-    CommonModule,
-    KonvaRoutingModule
-  ],
+    imports: [
+        CommonModule,
+        KonvaRoutingModule,
+        NgxFileDropModule
+    ],
 })
 export class KonvaModule { }
